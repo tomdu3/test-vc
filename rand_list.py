a8ed7bdab213b88638043f06db45b0e4d6c25eb8
Tomislav Dukez <tomdu3@ymail.com>
2024-09-06 18:41:24 +0200

    refactor rand_list to a function

--- rand_list.py
+++ rand_list.py
@@ -1,5 +1,8 @@
 import random
 
-random_list = random.sample(range(0, 100), 10)
+def random_list(limit):
+    if limit < 0:
+        return None
+    return random.sample(range(0, 100), limit)
 
-print(random_list)
+print(random_list(-1))
